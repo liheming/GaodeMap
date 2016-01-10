b310e4c81d30ede8b67d112b3cdd4bd59a278ae4
haily <1325789491@qq.com>
2016-01-10 20:02:56 +0800

    更新

---
+++ app/src/main/java/com/example/administrator/gaodemapabc/CameraSurface.java
@@ -0,0 +1,20 @@
+package com.example.administrator.gaodemapabc;
+
+import android.content.Context;
+
+/**
+ * Created by Base on 2015/11/24.
+ */
+public class CameraSurface {
+
+    private boolean ZOrderOnTop;
+
+    public CameraSurface(Context applicationContext) {
+
+
+    }
+
+    public void setZOrderOnTop(boolean ZOrderOnTop) {
+        this.ZOrderOnTop = ZOrderOnTop;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/gaodemapabc/MyUser.java
@@ -0,0 +1,7 @@
+package com.example.administrator.gaodemapabc;
+
+/**
+ * Created by haily on 2016/1/10.
+ */
+public class MyUser {
+}
